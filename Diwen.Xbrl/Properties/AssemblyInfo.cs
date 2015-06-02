a060b4c7a6654ae7793605c56f4cb792fe447461
John Nordberg <dgm9704@hotmail.com>
2015-06-03 02:52:37 +0300

    adding AssemblyInfo.cs that keeps disappearing

---
+++ Diwen.Xbrl/Properties/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Diwen.Xbrl")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("John Nordberg")]
+[assembly: AssemblyProduct("Diwen.Xbrl")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2aa5d6b4-86da-463a-bc79-905adccabcb8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("0.0.0.5")]
+[assembly: AssemblyFileVersion("0.0.0.5")]
+[assembly: CLSCompliant(true)]
