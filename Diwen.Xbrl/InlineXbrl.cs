a69b715268769771a5d9de9eefc4733c74686861
John Nordberg <john.nordberg@gmail.com>
2020-04-13 17:43:21 +0300

    #68 G2-4-1_3

--- Diwen.Xbrl/InlineXbrl.cs
+++ Diwen.Xbrl/InlineXbrl.cs
@@ -46,7 +46,149 @@ namespace Diwen.Xbrl
             G2_3_1_3,
             G2_4_1_1,
             G2_4_1_2,
+            G2_4_1_3,
         };
+
+
+        public static EsefResult ValidateEsef(string path)
+        => ValidateEsef(XDocument.Load(path));
+
+        public static EsefResult ValidateEsef(XDocument report)
+        {
+            var errors =
+                EsefValidations.
+                Select(validation => validation(report)).
+                Where(error => !string.IsNullOrWhiteSpace(error)).
+                ToArray();
+
+            return new EsefResult(errors.Any() ? "invalid" : "valid", errors.ToArray());
+        }
+
+        public static Instance ParseInstance(XDocument report)
+        {
+            var instance = new Instance();
+
+            ParseNamespaces(report, instance);
+
+            ParseSchemaReference(report, instance);
+
+            ParseContexts(report, instance);
+
+            ParseUnits(report, instance);
+
+            ParseFacts(report, instance);
+
+            return instance;
+        }
+
+        // public static ValidationResult ValidateEsef(string inputFile)
+        //  FormatValidations.GetValueOrDefault(InlineXbrlType.Esef, (f) => throw new ArgumentOutOfRangeException(nameof(InlineXbrlType.Esef)))(inputFile);
+
+        private static void ParseFacts(XDocument report, Instance instance)
+        {
+            var factElements = FindFacts(report);
+            var facts = new FactCollection(instance);
+            foreach (var factElement in factElements)
+            {
+                var name = factElement.Attribute("name").Value;
+                var prefix = name.Split(':').First();
+                var ns = report.Root.GetNamespaceOfPrefix(prefix).ToString();
+                var unitRef = factElement.Attribute("unitRef")?.Value;
+                var decimals = factElement.Attribute("decimals")?.Value;
+                var value = factElement.Value;
+                var scale = factElement.Attribute("scale")?.Value;
+
+                if (!string.IsNullOrWhiteSpace(scale))
+                {
+                    var power = int.Parse(scale);
+                    var v = decimal.Parse(value.Replace(" ", ""));
+                    var multiplier = (decimal)Math.Pow(10, power);
+                    v *= multiplier;
+                    value = v.ToString();
+                }
+
+                var contextRef = factElement.Attribute("contextRef").Value;
+                var fact = new Fact(name, ns, unitRef, decimals, contextRef, value);
+                facts.Add(fact);
+            }
+            foreach (var fact in facts)
+                instance.Facts.Add(fact);
+        }
+
+        private static IEnumerable<XElement> FindFacts(XDocument report)
+        {
+            // parse facts and add to instance
+            return report.Descendants().Where(d => d.Attribute("contextRef") != null);
+        }
+
+        private static void ParseUnits(XDocument report, Instance instance)
+        {
+            // parse units and add to instance
+            // ix:header/ix:resources/xbrli:unit
+            var unitNs = report.Root.GetNamespaceOfPrefix("xbrli");
+            var unitElements = report.Root.Descendants(unitNs + "unit");
+            var unitSerializer = new XmlSerializer(typeof(Unit));
+            var units = new UnitCollection(instance);
+            foreach (var unitElement in unitElements)
+            {
+                var unitReader = unitElement.CreateReader();
+                var unit = (Unit)unitSerializer.Deserialize(unitReader);
+                units.Add(unit);
+            }
+            instance.Units = units;
+        }
+
+        private static void ParseContexts(XDocument report, Instance instance)
+        {
+            // parse contexts and add to instance
+            // ix:header/ix:resources/xbrli:context
+            var contextNs = report.Root.GetNamespaceOfPrefix("xbrli");
+            var contextElements = report.Root.Descendants(contextNs + "context");
+            var contextSerializer = new XmlSerializer(typeof(Context));
+
+            var contexts = new ContextCollection(instance);
+            foreach (var contextElement in contextElements)
+            {
+                var contextReader = contextElement.CreateReader();
+                var context = (Context)contextSerializer.Deserialize(contextReader);
+                contexts.Add(context);
+            }
+            instance.Contexts = contexts;
+        }
+
+        private static void ParseSchemaReference(XDocument report, Instance instance)
+        {
+            // parse schemaRef and add to instance
+            // ix:header/ix:references/link:schemaRef
+            var link = report.Root.GetNamespaceOfPrefix("link");
+            // there can be only one
+            var schemaRefElements = report.Root.Descendants(link + "schemaRef");
+            var schemaRefSerializer = new XmlSerializer(typeof(SchemaReference));
+            var schemaRefElement = schemaRefElements.Single();
+            var schemaReader = schemaRefElement.CreateReader();
+            var schemaRef = (SchemaReference)schemaRefSerializer.Deserialize(schemaReader);
+            instance.SchemaReference = schemaRef;
+        }
+
+        private static void ParseNamespaces(XDocument report, Instance instance)
+        {
+            // parse namespaces from xhtml and add to instance
+            var namespaces =
+                report.
+                Root.
+                Attributes().
+                Where(a => a.IsNamespaceDeclaration).
+                Where(a => a.Name.LocalName != "xmlns").
+                ToDictionary(
+                    g => g.Name.LocalName,
+                    g => g.Value);
+
+            foreach (var ns in namespaces)
+                instance.Namespaces.AddNamespace(ns.Key, ns.Value);
+        }
+
+        public static Instance ParseInstance(string path)
+        => ParseInstance(XDocument.Load(path));
 
         private static string G2_1_2(XDocument report)
         {
@@ -242,144 +384,20 @@ namespace Diwen.Xbrl
             return errors.Join(",");
         }
 
-        public static EsefResult ValidateEsef(string path)
-        => ValidateEsef(XDocument.Load(path));
-
-        public static EsefResult ValidateEsef(XDocument report)
+        private static string G2_4_1_3(XDocument report)
         {
-            var errors =
-                EsefValidations.
-                Select(validation => validation(report)).
-                Where(error => !string.IsNullOrWhiteSpace(error)).
-                ToArray();
+            var errors = new HashSet<string>();
 
-            return new EsefResult(errors.Any() ? "invalid" : "valid", errors.ToArray());
+            var ix = report.Root.GetNamespaceOfPrefix("ix");
+            var fractions = report.Root.Descendants(ix + "fraction");
+
+            if (fractions.Any(t => (t.Attribute("name")?.Value ?? "").IndexOf(':') != -1))
+                errors.Add("fractionDefinedInExtensionTaxonomy");
+
+            if (fractions.Any())
+                errors.Add("fractionElementUsed");
+
+            return errors.Join(",");
         }
-
-        public static Instance ParseInstance(XDocument report)
-        {
-            var instance = new Instance();
-
-            ParseNamespaces(report, instance);
-
-            ParseSchemaReference(report, instance);
-
-            ParseContexts(report, instance);
-
-            ParseUnits(report, instance);
-
-            ParseFacts(report, instance);
-
-            return instance;
-        }
-
-        // public static ValidationResult ValidateEsef(string inputFile)
-        //  FormatValidations.GetValueOrDefault(InlineXbrlType.Esef, (f) => throw new ArgumentOutOfRangeException(nameof(InlineXbrlType.Esef)))(inputFile);
-
-        private static void ParseFacts(XDocument report, Instance instance)
-        {
-            var factElements = FindFacts(report);
-            var facts = new FactCollection(instance);
-            foreach (var factElement in factElements)
-            {
-                var name = factElement.Attribute("name").Value;
-                var prefix = name.Split(':').First();
-                var ns = report.Root.GetNamespaceOfPrefix(prefix).ToString();
-                var unitRef = factElement.Attribute("unitRef")?.Value;
-                var decimals = factElement.Attribute("decimals")?.Value;
-                var value = factElement.Value;
-                var scale = factElement.Attribute("scale")?.Value;
-
-                if (!string.IsNullOrWhiteSpace(scale))
-                {
-                    var power = int.Parse(scale);
-                    var v = decimal.Parse(value.Replace(" ", ""));
-                    var multiplier = (decimal)Math.Pow(10, power);
-                    v *= multiplier;
-                    value = v.ToString();
-                }
-
-                var contextRef = factElement.Attribute("contextRef").Value;
-                var fact = new Fact(name, ns, unitRef, decimals, contextRef, value);
-                facts.Add(fact);
-            }
-            foreach (var fact in facts)
-                instance.Facts.Add(fact);
-        }
-
-        private static IEnumerable<XElement> FindFacts(XDocument report)
-        {
-            // parse facts and add to instance
-            return report.Descendants().Where(d => d.Attribute("contextRef") != null);
-        }
-
-        private static void ParseUnits(XDocument report, Instance instance)
-        {
-            // parse units and add to instance
-            // ix:header/ix:resources/xbrli:unit
-            var unitNs = report.Root.GetNamespaceOfPrefix("xbrli");
-            var unitElements = report.Root.Descendants(unitNs + "unit");
-            var unitSerializer = new XmlSerializer(typeof(Unit));
-            var units = new UnitCollection(instance);
-            foreach (var unitElement in unitElements)
-            {
-                var unitReader = unitElement.CreateReader();
-                var unit = (Unit)unitSerializer.Deserialize(unitReader);
-                units.Add(unit);
-            }
-            instance.Units = units;
-        }
-
-        private static void ParseContexts(XDocument report, Instance instance)
-        {
-            // parse contexts and add to instance
-            // ix:header/ix:resources/xbrli:context
-            var contextNs = report.Root.GetNamespaceOfPrefix("xbrli");
-            var contextElements = report.Root.Descendants(contextNs + "context");
-            var contextSerializer = new XmlSerializer(typeof(Context));
-
-            var contexts = new ContextCollection(instance);
-            foreach (var contextElement in contextElements)
-            {
-                var contextReader = contextElement.CreateReader();
-                var context = (Context)contextSerializer.Deserialize(contextReader);
-                contexts.Add(context);
-            }
-            instance.Contexts = contexts;
-        }
-
-        private static void ParseSchemaReference(XDocument report, Instance instance)
-        {
-            // parse schemaRef and add to instance
-            // ix:header/ix:references/link:schemaRef
-            var link = report.Root.GetNamespaceOfPrefix("link");
-            // there can be only one
-            var schemaRefElements = report.Root.Descendants(link + "schemaRef");
-            var schemaRefSerializer = new XmlSerializer(typeof(SchemaReference));
-            var schemaRefElement = schemaRefElements.Single();
-            var schemaReader = schemaRefElement.CreateReader();
-            var schemaRef = (SchemaReference)schemaRefSerializer.Deserialize(schemaReader);
-            instance.SchemaReference = schemaRef;
-        }
-
-        private static void ParseNamespaces(XDocument report, Instance instance)
-        {
-            // parse namespaces from xhtml and add to instance
-            var namespaces =
-                report.
-                Root.
-                Attributes().
-                Where(a => a.IsNamespaceDeclaration).
-                Where(a => a.Name.LocalName != "xmlns").
-                ToDictionary(
-                    g => g.Name.LocalName,
-                    g => g.Value);
-
-            foreach (var ns in namespaces)
-                instance.Namespaces.AddNamespace(ns.Key, ns.Value);
-        }
-
-        public static Instance ParseInstance(string path)
-        => ParseInstance(XDocument.Load(path));
     }
 }
