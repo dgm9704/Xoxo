35dadfb311a29bed550ad371837d87499a3ecedb
John Nordberg <john.nordberg@gmail.com>
2021-07-26 21:10:07 +0300

    #73 start adding experimental support for xBRL-CSV

---
+++ Diwen.XbrlCsv.Tests/ReportTest.cs
@@ -0,0 +1,39 @@
+namespace Diwen.XbrlCsv.Tests
+{
+	using System.IO;
+	using System.Text.Json;
+	using Xunit;
+
+	public class ReportTest
+	{
+		[Fact]
+		public void Export()
+		{
+			var report = new Report();
+			report.DocumentInfo = new DocumentInfo();
+			report.DocumentInfo.Extends = "http://www.eba.europa.eu/eu/fr/xbrl/crr/fws/sbp/cir-2070-2016/2021-07-15/mod/sbp_cr_con.json";
+
+			report.FilingIndicators.Add("C_101.00", true);
+			report.FilingIndicators.Add("C_102.00", true);
+			report.FilingIndicators.Add("C_103.00", true);
+			report.FilingIndicators.Add("C_105.01", true);
+			report.FilingIndicators.Add("C_105.02", true);
+			report.FilingIndicators.Add("C_105.03", true);
+			report.FilingIndicators.Add("C_111.00", true);
+			report.FilingIndicators.Add("C_112.00", true);
+			report.FilingIndicators.Add("C_113.00", true);
+			report.FilingIndicators.Add("C_114.00", true);
+			report.FilingIndicators.Add("S_00.01", true);
+
+			report.Export();
+		}
+
+		[Fact]
+		public void Import()
+		{
+			string fileName = "report.json";
+			string jsonString = File.ReadAllText(fileName);
+			var report = JsonSerializer.Deserialize<Report>(jsonString);
+		}
+	}
+}
---
+++ Diwen.XbrlCsv/Report.cs
@@ -0,0 +1,34 @@
+﻿namespace Diwen.XbrlCsv
+{
+	using System;
+	using System.Collections.Generic;
+	using System.IO;
+	using System.Linq;
+	using System.Text;
+	using System.Text.Json;
+	using System.Text.Json.Serialization;
+
+	public class Report
+	{
+		[JsonPropertyName("documentInfo")]
+		public DocumentInfo DocumentInfo = new DocumentInfo();
+
+		[JsonIgnore]
+		public Dictionary<string, bool> FilingIndicators = new Dictionary<string, bool>();
+
+		public void Export()
+		{
+			string fileName = "report.json";
+			var options = new JsonSerializerOptions { WriteIndented = true };
+			string jsonString = JsonSerializer.Serialize<Report>(this, options);
+			File.WriteAllText(fileName, jsonString);
+
+			fileName = "FilingIndicators.csv";
+			var data = new StringBuilder();
+			data.AppendLine("templateId,reported");
+			foreach (var fi in FilingIndicators)
+				data.AppendLine($"{fi.Key},{fi.Value.ToString().ToLower()}");
+			File.WriteAllText(fileName, data.ToString());
+		}
+	}
+}
