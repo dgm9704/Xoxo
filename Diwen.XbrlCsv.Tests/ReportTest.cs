c37d2f057b284bb762c780f754165d8189a9970c
John Nordberg <john.nordberg@gmail.com>
2021-07-26 22:10:08 +0300

    #73 start adding experimental support for xBRL-CSV

--- Diwen.XbrlCsv.Tests/ReportTest.cs
+++ Diwen.XbrlCsv.Tests/ReportTest.cs
@@ -10,8 +10,15 @@ namespace Diwen.XbrlCsv.Tests
 		public void Export()
 		{
 			var report = new Report();
-			report.DocumentInfo = new DocumentInfo();
 			report.DocumentInfo.Extends = "http://www.eba.europa.eu/eu/fr/xbrl/crr/fws/sbp/cir-2070-2016/2021-07-15/mod/sbp_cr_con.json";
+
+			report.Parameters.Add("entityID", "lei:DUMMYLEI123456789012");
+			report.Parameters.Add("refPeriod", "2021-12-31");
+			report.Parameters.Add("baseCurrency", "iso4217:EUR");
+			report.Parameters.Add("decimalsInteger", "0");
+			report.Parameters.Add("decimalsMonetary", "-3");
+			report.Parameters.Add("decimalsPercentage", "4");
+			report.Parameters.Add("decimalsDecimal", "2");
 
 			report.FilingIndicators.Add("C_101.00", true);
 			report.FilingIndicators.Add("C_102.00", true);
--- Diwen.XbrlCsv/Report.cs
+++ Diwen.XbrlCsv/Report.cs
@@ -1,9 +1,7 @@
 ﻿namespace Diwen.XbrlCsv
 {
-	using System;
 	using System.Collections.Generic;
 	using System.IO;
-	using System.Linq;
 	using System.Text;
 	using System.Text.Json;
 	using System.Text.Json.Serialization;
@@ -11,24 +9,53 @@
 	public class Report
 	{
 		[JsonPropertyName("documentInfo")]
-		public DocumentInfo DocumentInfo = new DocumentInfo();
+		public DocumentInfo DocumentInfo { get; set; }// = new DocumentInfo();
 
 		[JsonIgnore]
 		public Dictionary<string, bool> FilingIndicators = new Dictionary<string, bool>();
 
+		[JsonIgnore]
+		public Dictionary<string, string> Parameters = new Dictionary<string, string>();
+
+		public Report()
+		=> DocumentInfo = new DocumentInfo();
+
 		public void Export()
+		{
+			ExportDocumentInfo();
+
+			ExportParameters();
+
+			ExportFilingIndicators();
+		}
+
+		private void ExportDocumentInfo()
 		{
 			string fileName = "report.json";
 			var options = new JsonSerializerOptions { WriteIndented = true };
 			string jsonString = JsonSerializer.Serialize<Report>(this, options);
 			File.WriteAllText(fileName, jsonString);
+		}
 
-			fileName = "FilingIndicators.csv";
+		private void ExportParameters()
+		{
+			var fileName = "parameters.csv";
+			var data = new StringBuilder();
+			data.AppendLine("name,value");
+			foreach (var p in Parameters)
+				data.AppendLine($"{p.Key},{p.Value}");
+			File.WriteAllText(fileName, data.ToString());
+		}
+
+		private void ExportFilingIndicators()
+		{
+			var fileName = "FilingIndicators.csv";
 			var data = new StringBuilder();
 			data.AppendLine("templateId,reported");
 			foreach (var fi in FilingIndicators)
 				data.AppendLine($"{fi.Key},{fi.Value.ToString().ToLower()}");
 			File.WriteAllText(fileName, data.ToString());
 		}
+
 	}
 }
