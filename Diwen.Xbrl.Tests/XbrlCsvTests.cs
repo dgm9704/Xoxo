9adec052c4e5c03b9297af0e6eaff8a943c73997
John Nordberg <john.nordberg@gmail.com>
2024-04-14 20:41:02 +0300

    fiddling

--- Diwen.Xbrl.Tests/XbrlCsvTests.cs
+++ Diwen.Xbrl.Tests/XbrlCsvTests.cs
@@ -22,27 +22,33 @@ namespace Diwen.Xbrl.Csv.Tests
         [Fact]
         public void ExportTests()
         {
-            var report = new Report();
-            report.Entrypoint = "http://www.eba.europa.eu/eu/fr/xbrl/crr/fws/sbp/cir-2070-2016/2021-07-15/mod/sbp_cr_con.json";
-
-            report.Parameters.Add("entityID", "lei:DUMMYLEI123456789012");
-            report.Parameters.Add("refPeriod", "2021-12-31");
-            report.Parameters.Add("baseCurrency", "iso4217:EUR");
-            report.Parameters.Add("decimalsInteger", "0");
-            report.Parameters.Add("decimalsMonetary", "-3");
-            report.Parameters.Add("decimalsPercentage", "4");
-            report.Parameters.Add("decimalsDecimal", "2");
-
-            report.FilingIndicators.Add("C_101.00", false);
-            report.FilingIndicators.Add("C_102.00", false);
-            report.FilingIndicators.Add("C_103.00", false);
-            report.FilingIndicators.Add("C_105.01", false);
-            report.FilingIndicators.Add("C_105.02", true);
-            report.FilingIndicators.Add("C_105.03", true);
-            report.FilingIndicators.Add("C_111.00", false);
-            report.FilingIndicators.Add("C_112.00", false);
-            report.FilingIndicators.Add("C_113.00", true);
-            report.FilingIndicators.Add("S_00.01", true);
+            var report = new Report
+            {
+                Entrypoint = "http://www.eba.europa.eu/eu/fr/xbrl/crr/fws/sbp/cir-2070-2016/2021-07-15/mod/sbp_cr_con.json",
+                Parameters = new Dictionary<string, string>
+                {
+                    ["entityID"] = "lei:DUMMYLEI123456789012",
+                    ["refPeriod"] = "2021-12-31",
+                    ["baseCurrency"] = "iso4217:EUR",
+                    ["decimalsInteger"] = "0",
+                    ["decimalsMonetary"] = "-3",
+                    ["decimalsPercentage"] = "4",
+                    ["decimalsDecimal"] = "2",
+                },
+                FilingIndicators = new Dictionary<string, bool>
+                {
+                    ["C_101.00"] = false,
+                    ["C_102.00"] = false,
+                    ["C_103.00"] = false,
+                    ["C_105.01"] = false,
+                    ["C_105.02"] = true,
+                    ["C_105.03"] = true,
+                    ["C_111.00"] = false,
+                    ["C_112.00"] = false,
+                    ["C_113.00"] = true,
+                    ["S_00.01"] = true,
+                },
+            };
 
             //datapoint,factValue
             report.AddData("S_00.01", "dp31870", "eba_AS:x1");
